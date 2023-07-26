83aab9385acd99d3e007551125715240212848d7
Andrew <93514394+JustRey-007@users.noreply.github.com>
2023-07-26 17:19:48 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function hoverButton() {
+    document.querySelector('.btn-join').classList.add('btn-join-hovered');
+}
+
+function unhoverButton() {
+    document.querySelector('.btn-join').classList.remove('btn-join-hovered');
+}
+
+function returnSite() {
+    location.reload();
+}
